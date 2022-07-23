2c8b05ad3d29dd18ed3c3bac2f7234fb9268c0de
palec87 <david@stanka.de>
2022-07-23 14:33:07 +0100

    heroku files 05

--- app/__init__.py
+++ app/__init__.py
@@ -3,45 +3,46 @@ import os
 from flask import Flask
 from .gallery.probability import prob_pee_sea
 from .gallery.quadratic import quad_pizza
+from . import routes
+from . import gallery
 
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev')
+# def create_app(test_config=None):
+app = Flask(__name__)  # , instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev')
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# if test_config is None:
+#     # load the instance config, if it exists, when not testing
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     # load the test config if passed in
+#     app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # # root
-    from . import routes
-    app.register_blueprint(routes.bp_home)
-    app.add_url_rule('/', endpoint='home')
+# # root
+app.register_blueprint(routes.bp_home)
+app.add_url_rule('/', endpoint='home')
 
-    app.register_blueprint(routes.bp_contact)
-    app.add_url_rule('/contact', endpoint='contact')
+app.register_blueprint(routes.bp_contact)
+app.add_url_rule('/contact', endpoint='contact')
 
-    app.register_blueprint(routes.bp_todo)
-    app.add_url_rule('/not-implemented', endpoint='not-implemented')
+app.register_blueprint(routes.bp_todo)
+app.add_url_rule('/not-implemented', endpoint='not-implemented')
 
-    # gallery
-    from . import gallery
-    app.register_blueprint(gallery.bp_gal)
+# gallery
+app.register_blueprint(gallery.bp_gal)
 
-    app.register_blueprint(gallery.bp_demo)
+app.register_blueprint(gallery.bp_demo)
 
-    # dash plotly apps
-    app = prob_pee_sea(app)
-    app = quad_pizza(app)
+# dash plotly apps
+app = prob_pee_sea(app)
+app = quad_pizza(app)
 
-    return app
+if __name__ == "__main__":
+    app.run()
